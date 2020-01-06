f1224e1d11443e32034a8d17be706fa17c81941d
Túlio Paim <tulio.paim@mw.far.br>
2020-01-06 17:53:41 -0300

    Separa arquivos

---
+++ ConsoleApp/ClassExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public static class ClassExtension
+    {
+        public static T CloneObject<T>(this T obj)
+        {
+            T newObj = (T)Activator.CreateInstance(typeof(T));
+            var properties = GetProperties(typeof(T));
+
+            foreach (var property in properties)
+            {
+                var propInfo = typeof(T).GetProperty(property.Name);
+                var value = propInfo.GetValue(obj);
+                if (value != null)
+                {
+                    propInfo.SetValue(newObj, value);
+                }
+            }
+
+            return newObj;
+        }
+
+        public static List<Variable> GetProperties(Type type)
+        {
+            var propertyValues = type.GetProperties();
+            var result = new List<Variable>();
+
+            foreach (var property in propertyValues)
+            {
+                result.Add(new Variable
+                {
+                    Name = property.Name,
+                    Type = property.PropertyType,
+                });
+            }
+
+            return result;
+        }
+
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -103,43 +103,4 @@ namespace ConsoleApp
         }
     }
 
-    public static class ClassExtention
-    {
-        public static T CloneObject<T>(this T obj)
-        {
-            T newObj = (T)Activator.CreateInstance(typeof(T));
-            var properties = GetProperties(typeof(T));
-
-            foreach (var property in properties)
-            {
-                var propInfo = typeof(T).GetProperty(property.Name);
-                var value = propInfo.GetValue(obj);
-                if(value != null)
-                {
-                    propInfo.SetValue(newObj, value);
-                }
-            }
-
-            return newObj;
-        }
-
-        public static List<Variable> GetProperties(Type type)
-        {
-            var propertyValues = type.GetProperties();
-            var result = new List<Variable>();
-
-            foreach (var property in propertyValues)
-            {
-                result.Add(new Variable
-                {
-                    Name = property.Name,
-                    Type = property.PropertyType,
-                });
-            }
-
-            return result;
-        }
-
-    }
-
 }
